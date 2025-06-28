2962dbc67024d444b80a9907abe6c153de504346
Your Name <you@example.com>
2025-06-28 14:29:39 +0200

    add comments while getting fibonacci numbers

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fibonacci import caching_fibonacci
 
 if __name__ == "__main__":
     print(" === Task 1. Caching fibonacci === ")
+
+    # get the fibonacci function
     fib = caching_fibonacci()
     print("The value of fibonacci sequence with the index 15 is")
+
+    # use the function for getting fibonacci numbers
     print(fib(15))
+    print(fib(10))
